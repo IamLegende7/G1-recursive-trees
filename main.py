a86fbac60ab0323872e16c6b27decb70aa641d88
dl2x <johann@quast.net>
2025-11-19 09:31:35 +0100

    made better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 
-debug = False
+debug = True
 file = 'image.svg'
 
 ## SETTINGS ##
@@ -10,18 +10,18 @@ width = 1000
 
 # Size
 initial_size = 200
-dropoff = 0.5
+dropoff = 1.5
+exponential_dropoff = True
 
 # Generations & Children
-max_generations = 5
-initial_children_count = 2
+max_generations = 15
+children_count = 2
 line_width = 4
 
 # Spread
 spread = 45
-spread_dropoff = 1
-do_spread_dropoff = False
 keep_rotation = True
+exponential_length = False
 
 
 def toFile(contents):
@@ -30,51 +30,31 @@ def toFile(contents):
     except: 
       print("Error writing to svg file!")
 
-def node(startx, starty, generation, child_num, children_count_parent, rotation = None):
+def node(startx, starty, generation, alpha, length):
   
+  if exponential_dropoff: new_length = length / dropoff
+  else: new_length = initial_size / (generation * dropoff + 1)
+  x_offset = math.sin(alpha * (math.pi / 180)) * new_length
+  y_offset = math.cos(alpha * (math.pi / 180)) * new_length
+
   ## Mathe
-  child_multi = child_num - math.ceil(children_count_parent / 2)
-  if ((children_count_parent % 2) == 0):
-    if child_num <= (children_count_parent / 2): child_multi -= 1
-    if child_multi < 0: child_multi += 0.5
-    else: child_multi -= 0.5
 
-  if generation == 0:
-    alpha = 0
-  else:
-    if do_spread_dropoff: spread_full = (spread / (generation / (1 / spread_dropoff)))
-    else:                 spread_full = spread
+  dist = 180 / children_count
 
-    if (rotation is None) or (not keep_rotation):
-      alpha = (            spread_full * child_multi)
-    else:
-      alpha = (rotation + (spread_full * child_multi))
-
-  alpha_rad = alpha * (math.pi / 180)
-
-  if debug: print(f"Gen: {generation} Alpha: {alpha}")
-
-  x_offset = math.sin(alpha_rad) * (initial_size / (dropoff * (generation + 1)))
-  y_offset = math.cos(alpha_rad) * (initial_size / (dropoff * (generation + 1)))
+  if debug: print(f"Gen: {generation} Dist: {dist}")
 
   endx = startx + x_offset
   endy = starty + y_offset
 
-
   ## Drawing
   drawLine(startx,starty,endx,endy,(line_width / (generation + 1)))
-  #svg_writer.write(svg_writer.line(startx, starty, endx, endy, (line_width / (generation + 1)), "#000000"))
 
-  if generation < max_generations:
-    ## Get child count
-    if generation == 0:
-      children_count = initial_children_count
-    else:
-      children_count = children_count_parent
+  if generation > max_generations: return
 
-    ## Generate Children
-    for i in range(children_count):
-      node(endx, endy, generation+1, i+1, children_count, rotation=alpha)
+  ## Generate Children
+  for i in range(children_count):
+    cur_rot = (i+0.5) * dist + alpha - 90
+    node(endx, endy, generation+1, cur_rot, new_length)
 
 def drawLine(x1,y1,x2,y2,width):
   toFile(f'<line x1="{str(x1)}" y1="{str(y1)}" x2="{str(x2)}" y2="{str(y2)}" stroke="#000000" stroke-width="{str(width)}" />')
@@ -92,7 +72,7 @@ initFile(file)
 
 
 # Generating Tree
-node(startx=(width / 2), starty=0, generation=0, child_num=1, children_count_parent=1)
+node((width / 2), 0, 0, 0, initial_size*2)
 
 # Cleanup
 toFile('</svg>')
